c4a69c427bfeabcc16dba48d035baa5b69814fcd
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-03-26 15:46:43 +0000

    feat: added traffic light to Home

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,23 +4,12 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+import TrafficLight from "./TrafficLight";
+
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center">
+			<TrafficLight />
 		</div>
 	);
 };
